d33ee124b04e0fd10a3e264492985960e3f189ef
Dedaldino Daniel <dedaldinodev4@gmail.com>
2023-10-09 10:47:01 +0100

    feat: Repository-component

--- src/app/components/GitHubRepo/index.tsx
+++ src/app/components/GitHubRepo/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { api  } from '@/app/services/api';
+import axios from 'axios';
+
+import { Repository } from '../Repository';
 
 export interface IGitHubUser {
   username: string;
 }
 export const GitHubRepositories: React.FC<IGitHubUser> = async ({ username }: IGitHubUser) => {
 
-  const response = await api.get(`users/${username}/repos`)
-  const { data } = response;
+  //const response = await api.get(`users/${username}/repos`)
+  //const { data } = response;
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos/');
+  const { data } = response
   console.log(data)
 
   return (
-    <>
-
-    </>
+    <Repository />
   );
 }
--- src/app/repos/page.tsx
+++ src/app/repos/page.tsx
@@ -7,9 +7,8 @@ export default function Repos() {
   const { user } = useContext(GitHubRepoContext)
 
   return (
-    <main className="bg-gray-100 flex justify-center items-center h-screen">
-     <h1 className='text-lg text-center'>Hello Repos</h1>
+    <>
      <GitHubRepositories username={user.username}/>
-    </main>
+    </>
   )
 }
